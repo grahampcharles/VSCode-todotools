452e2fb43cfb3a78c1e73dec6be9f1d9a287c49f
Graham Charles <grahampcharles@gmail.com>
2021-12-06 03:51:51 +0000

    adding cleanYaml

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,120 +1,135 @@
-import * as assert from 'assert';
-import * as vscode from 'vscode';
-import * as chai from 'chai';
-import { dayNames, dayNameToWeekday, daysPassed, todayDay, todayName } from '../../dates';
-import { isCurrentRecurringItem, parseYamlTasks, RecurringTask, yamlToTask } from '../../yaml-utilities';
-import { expect } from 'chai';
-import { testYaml, testYamlTasks, testYamlToday } from './testdata';
-import YAML = require('yaml');
-import { Settings } from '../../Settings';
-import dayjs = require('dayjs');
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as chai from "chai";
+import {
+  dayNames,
+  dayNameToWeekday,
+  daysPassed,
+  todayDay,
+  todayName,
+} from "../../dates";
+import {
+  cleanYaml,
+  isCurrentRecurringItem,
+  parseYamlTasks,
+  RecurringTask,
+  yamlToTask,
+} from "../../yaml-utilities";
+import { expect } from "chai";
+import { testYaml, testYamlTasks, testYamlToday } from "./testdata";
+import YAML = require("yaml");
+import { Settings } from "../../Settings";
+import dayjs = require("dayjs");
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test('date functions', () => {
-		const date1 = new Date(2020, 1, 3);
-		const date2 = new Date(2020, 1, 5);
-		assert.strictEqual(2, daysPassed(date1, date2));
+  test("date functions", () => {
+    const date1 = new Date(2020, 1, 3);
+    const date2 = new Date(2020, 1, 5);
+    assert.strictEqual(2, daysPassed(date1, date2));
 
-		// day names
-		assert.strictEqual("Sunday", dayNames[0]);
-		assert.strictEqual(0, dayNameToWeekday("Sunday"));
+    // day names
+    assert.strictEqual("Sunday", dayNames[0]);
+    assert.strictEqual(0, dayNameToWeekday("Sunday"));
 
-		// today's day name
-		assert.strictEqual(todayName, dayNames[todayDay.day()]);
+    // today's day name
+    assert.strictEqual(todayName, dayNames[todayDay.day()]);
+  });
 
-	});
+  test("yaml settings", () => {
+    let settings = new Settings();
 
-	test('yaml settings', () => {
-		let settings = new Settings();
+    // empty settings
+    expect(settings.hasRunToday()).to.be.false;
+    expect(settings.runOnOpen).to.be.false;
+    expect(settings.runDaily).to.be.false;
+    expect(settings.lastAutoRun.isValid()).to.be.false;
 
-		// empty settings
-		expect(settings.hasRunToday()).to.be.false;
-		expect(settings.runOnOpen).to.be.false;
-		expect(settings.runDaily).to.be.false;
-		expect(settings.lastAutoRun.isValid()).to.be.false;
+    // default settings
+    settings.readFromYaml(testYaml);
+    expect(settings.hasRunToday(), "default settings: hasRunToday").to.be.false;
+    expect(settings.runOnOpen, "default settings: runOnOpen").to.be.true;
+    expect(settings.runDaily, "default settings: runDaily").to.be.true;
 
-		// default settings
-		settings.readFromYaml(testYaml);
-		expect(settings.hasRunToday(), "default settings: hasRunToday").to.be.false;
-		expect(settings.runOnOpen, "default settings: runOnOpen").to.be.true;
-		expect(settings.runDaily, "default settings: runDaily").to.be.true;
+    // change to today
+    settings.readFromYaml(testYamlToday);
+    expect(settings.hasRunToday(), "today: hasRunToday").to.be.true;
+  });
 
-		// change to today
-		settings.readFromYaml(testYamlToday);
-		expect(settings.hasRunToday(), "today: hasRunToday").to.be.true;
+  test("convert yaml to task", () => {
+    expect(yamlToTask("2")).property("recurAfter").eql(2, "recurAfter");
+    expect(yamlToTask("Monday")).property("dayOfWeek").eql(1, "dayOfWeek");
+    expect(yamlToTask("Mondays"))
+      .property("dayOfWeek")
+      .eql(1, "dayOfWeek-pluralized");
+    expect(yamlToTask("Jan. 2, 2001"))
+      .property("dateOnce")
+      .eql("2001-01-02", "dateOnce");
+    expect(yamlToTask("1/2")).property("dateAnnual").eql("01-02", "dateAnnual");
+    expect(yamlToTask("monthly")).property("dayOfMonth").eql(1, "dayOfMonth");
+  });
 
-	});
+  test("yaml cleaning", () => {
+    expect(cleanYaml(`\t- yaml item`)).eq(
+      "   - yaml item",
+      "convert tabs to spaces"
+    );
+  });
 
-	test('convert yaml to task', () => {
+  test("yaml parsing", () => {
+    const tasks = parseYamlTasks(testYamlTasks);
+    const yamlTaskArray = testYamlTasks.split("\r\n");
 
-		expect(yamlToTask('2')).property('recurAfter').eql(2, "recurAfter");
-		expect(yamlToTask('Monday')).property('dayOfWeek').eql(1, "dayOfWeek");
-		expect(yamlToTask('Mondays')).property('dayOfWeek').eql(1, "dayOfWeek-pluralized");
-		expect(yamlToTask('Jan. 2, 2001')).property('dateOnce').eql("2001-01-02", "dateOnce");
-		expect(yamlToTask('1/2')).property('dateAnnual').eql("01-02", "dateAnnual");
-		expect(yamlToTask('monthly')).property('dayOfMonth').eql(1, "dayOfMonth");
+    assert.strictEqual(tasks[0].name, "eat groceries");
+    assert.strictEqual(tasks[0].recurAfter, 2, "tasks-0-recurAfter");
+    assert.strictEqual(tasks[2].name, "mow lawn");
+    assert.strictEqual(tasks[2].recurAfter, 1);
 
-	});
+    const shopping = tasks.filter((e) => e.name === "shop");
+    expect(shopping).to.have.lengthOf(1, "shopping");
+    expect(shopping[0]).property("dayOfWeek").eql(2, "shopping: Tuesday");
 
-	test('yaml parsing', () => {
+    const taxes = tasks.filter((e) => e.name === "pay taxes");
+    expect(taxes).to.have.lengthOf(2, "taxes-lengthOf");
+    expect(taxes[0]).property("dateOnce").eql("2021-11-14");
+    expect(taxes[1]).property("dateOnce").eql("2022-04-10");
 
-		const tasks = parseYamlTasks(testYamlTasks);
-		const yamlTaskArray = testYamlTasks.split("\r\n");
+    const xmas = tasks.filter((e) => e.name === "start XMas shopping")[0];
+    expect(xmas).property("dateAnnual").eql("12-01");
 
-		assert.strictEqual(tasks[0].name, "eat groceries");
-		assert.strictEqual(tasks[0].recurAfter, 2, 'tasks-0-recurAfter');
-		assert.strictEqual(tasks[2].name, "mow lawn");
-		assert.strictEqual(tasks[2].recurAfter, 1);
+    const rent = tasks.filter((e) => e.name === "pay rent")[0];
+    expect(rent).property("dayOfMonth").eql(1, "dayOfMonth");
+  });
 
-		const shopping = tasks.filter(e => e.name === "shop");
-		expect(shopping).to.have.lengthOf(1, "shopping");
-		expect(shopping[0]).property("dayOfWeek").eql(2, "shopping: Tuesday");
+  test("recurring item checking", () => {
+    let testItem: RecurringTask = {
+      name: "test task",
+      dateOnce: todayDay.format("YYYY-MM-DD"),
+    };
+    const yesterday = todayDay.subtract(1, "day");
 
-		const taxes = tasks.filter(e => e.name === "pay taxes");
-		expect(taxes).to.have.lengthOf(2, 'taxes-lengthOf');
-		expect(taxes[0]).property("dateOnce").eql("2021-11-14");
-		expect(taxes[1]).property("dateOnce").eql("2022-04-10");
+    // dateOnce is today
+    expect(isCurrentRecurringItem(testItem)).to.be.true;
 
-		const xmas = tasks.filter(e => e.name === "start XMas shopping")[0];
-		expect(xmas).property("dateAnnual").eql("12-01");
+    // dateOnce is not today
+    testItem.dateOnce = yesterday.format("YYYY-MM-DD");
+    expect(isCurrentRecurringItem(testItem)).to.be.false;
+    delete testItem.dateOnce;
 
-		const rent = tasks.filter(e => e.name === "pay rent")[0];
-		expect(rent).property("dayOfMonth").eql(1, "dayOfMonth");
+    // dateAnnual is today
+    testItem.dateAnnual = todayDay.format("MM-DD");
+    expect(isCurrentRecurringItem(testItem)).to.be.true;
 
-	});
+    // dateAnnual is not today
+    testItem.dateAnnual = yesterday.format("MM-DD");
+    expect(isCurrentRecurringItem(testItem)).to.be.false;
+    delete testItem.dateAnnual;
 
-	test('recurring item checking', () => {
-
-		let testItem: RecurringTask = {
-			name: "test task",
-			dateOnce: todayDay.format("YYYY-MM-DD")
-		};
-		const yesterday = todayDay.subtract(1, "day");
-
-		// dateOnce is today
-		expect(isCurrentRecurringItem(testItem)).to.be.true;
-
-		// dateOnce is not today
-		testItem.dateOnce = yesterday.format("YYYY-MM-DD");
-		expect(isCurrentRecurringItem(testItem)).to.be.false;
-		delete testItem.dateOnce;
-
-		// dateAnnual is today
-		testItem.dateAnnual = todayDay.format("MM-DD");
-		expect(isCurrentRecurringItem(testItem)).to.be.true;
-
-		// dateAnnual is not today
-		testItem.dateAnnual = yesterday.format("MM-DD");
-		expect(isCurrentRecurringItem(testItem)).to.be.false;
-		delete testItem.dateAnnual;
-
-		// dayOfWeek is today's
-		testItem.dayOfWeek = todayDay.get('day');
-		expect(isCurrentRecurringItem(testItem)).to.be.true;
-		testItem.dayOfWeek = yesterday.get('day');
-		expect(isCurrentRecurringItem(testItem)).to.be.false;
-
-	});
+    // dayOfWeek is today's
+    testItem.dayOfWeek = todayDay.get("day");
+    expect(isCurrentRecurringItem(testItem)).to.be.true;
+    testItem.dayOfWeek = yesterday.get("day");
+    expect(isCurrentRecurringItem(testItem)).to.be.false;
+  });
 });
--- src/yaml-utilities.ts
+++ src/yaml-utilities.ts
@@ -1,8 +1,8 @@
 import * as vscode from "vscode";
-import YAML = require('yaml');
-import dayjs = require('dayjs');
-import utc = require('dayjs/plugin/utc');
-import timezone = require('dayjs/plugin/timezone');
+import YAML = require("yaml");
+import dayjs = require("dayjs");
+import utc = require("dayjs/plugin/utc");
+import timezone = require("dayjs/plugin/timezone");
 
 // work in the local time zone
 dayjs.extend(utc);
@@ -10,27 +10,39 @@ dayjs.extend(timezone);
 dayjs.tz.guess();
 
 import { yamlDelimiter } from "./constants";
-import { dayNameToWeekday, daysSinceTheBeginningOfTime, todayDay } from "./dates";
-
+import {
+  dayNameToWeekday,
+  daysSinceTheBeginningOfTime,
+  todayDay,
+} from "./dates";
 
 export function getYamlSection(editor: vscode.TextEditor): string[] {
-    var sectionLines: string[] = [];
-    var isInSection: Boolean = false;
+  var sectionLines: string[] = [];
+  var isInSection: Boolean = false;
 
-    for (let i = 0; i < editor.document.lineCount; i++) {
-        if (editor.document.lineAt(i).text === yamlDelimiter) {
-            isInSection = !isInSection;
-        } else if (/\S/.test(editor.document.lineAt(i).text)) {
-            // something other than whitespace?
-            if (isInSection) {
-                sectionLines.push(editor.document.lineAt(i).text);
-            }
-        }
+  for (let i = 0; i < editor.document.lineCount; i++) {
+    if (editor.document.lineAt(i).text === yamlDelimiter) {
+      isInSection = !isInSection;
+    } else if (/\S/.test(editor.document.lineAt(i).text)) {
+      // something other than whitespace?
+      if (isInSection) {
+        sectionLines.push(editor.document.lineAt(i).text);
+      }
     }
+  }
 
-    return sectionLines;
+  // TODO: clean the yaml a bit
+  return sectionLines;
 }
 
+// TODO: implement cleanYaml
+// should:
+//  -- remove illegal characters within the yaml, like tab and colon (?)
+export function cleanYaml(input: string): string {
+  // escape certain characters
+
+  return input;
+}
 
 /* 
     yaml format:
@@ -51,14 +63,13 @@ export function getYamlSection(editor: vscode.TextEditor): string[] {
 */
 
 export type RecurringTask = {
-    name?: string,
-    recurAfter?: number,     // every n days (1 = every day, 2 = every other, etc.)
-    dateAnnual?: string,     // date without a year, in YYYY-MM-DD format
-    dateOnce?: string,       // date with a year, in YYYY-MM-DD
-    dayOfWeek?: number,      // day of week
-    dayOfMonth?: number      // day of month
+  name?: string;
+  recurAfter?: number; // every n days (1 = every day, 2 = every other, etc.)
+  dateAnnual?: string; // date without a year, in YYYY-MM-DD format
+  dateOnce?: string; // date with a year, in YYYY-MM-DD
+  dayOfWeek?: number; // day of week
+  dayOfMonth?: number; // day of month
 };
-
 
 /**
  * Returns a boolean indicating whether this task should be added today.
@@ -68,70 +79,91 @@ export type RecurringTask = {
  * @return {*}  {boolean}
  */
 export function isCurrentRecurringItem(task: RecurringTask): boolean {
+  if (
+    task.recurAfter !== undefined &&
+    daysSinceTheBeginningOfTime % task.recurAfter === 0
+  ) {
+    return true;
+  }
+  if (
+    task.dateAnnual !== undefined &&
+    dayjs(task.dateAnnual).format("MM-DD") === dayjs().format("MM-DD")
+  ) {
+    return true;
+  }
+  if (
+    task.dateOnce !== undefined &&
+    dayjs(task.dateOnce).format("YYYY-MM-DD") === dayjs().format("YYYY-MM-DD")
+  ) {
+    return true;
+  }
+  if (task.dayOfWeek !== undefined && todayDay.day() === task.dayOfWeek) {
+    return true;
+  }
+  if (task.dayOfMonth !== undefined && todayDay.date() === task.dayOfMonth) {
+    return true;
+  }
 
-    if (task.recurAfter !== undefined && (daysSinceTheBeginningOfTime % task.recurAfter === 0)) { return true; };
-    if (task.dateAnnual !== undefined && (dayjs(task.dateAnnual).format("MM-DD") === dayjs().format("MM-DD"))) { return true; }
-    if (task.dateOnce !== undefined && (dayjs(task.dateOnce).format("YYYY-MM-DD") === dayjs().format("YYYY-MM-DD"))) { return true; }
-    if (task.dayOfWeek !== undefined && (todayDay.day() === task.dayOfWeek)) { return true; }
-    if (task.dayOfMonth !== undefined && (todayDay.date() === task.dayOfMonth)) { return true; }
-
-    return false;
-
-};
+  return false;
+}
 
 // TODO: allow duplicate-named tasks
 export function parseYamlTasks(yamlSection: string): RecurringTask[] {
+  const tree = YAML.parse(yamlSection);
 
-    const tree = YAML.parse(yamlSection);
+  // get the tasks section
+  if (!(tree && "tasks" in tree)) {
+    return [];
+  }
+  const tasks = tree["tasks"];
 
-    // get the tasks section
-    if (!(tree && "tasks" in tree)) { return []; }
-    const tasks = tree["tasks"];
-
-    return Object.keys(tasks).flatMap((pattern: string) => {
-        const taskProps = yamlToTask(pattern);
-        return tasks[pattern].map((key: string) => {
-            return ({ name: key, ...taskProps } as RecurringTask);
-        }
-        );
+  return Object.keys(tasks).flatMap((pattern: string) => {
+    const taskProps = yamlToTask(pattern);
+    return tasks[pattern].map((key: string) => {
+      return { name: key, ...taskProps } as RecurringTask;
     });
+  });
 }
 
 type TaskInputType = number | string;
 
 export function yamlToTask(input: TaskInputType): RecurringTask {
+  // is this a number | numeric string?
+  if (!isNaN(+input)) {
+    return { recurAfter: +input };
+  }
 
-    // is this a number | numeric string?
-    if (!isNaN(+input)) { return { recurAfter: +input }; }
-
-    if (typeof input === "string") {
-
-        // certain constants
-        if (input.toLowerCase() === "daily") { return { recurAfter: 1 }; }
-        if (input.toLowerCase() === "monthly") { return { dayOfMonth: 1 }; }
-
-        // day of week
-        const dayOfWeek = dayNameToWeekday(input);
-        if (dayOfWeek !== -1) { return { dayOfWeek: dayOfWeek }; };
-
-        // date without a year
-        if (input.length <= 5) {
-            // concatenating 1600 to the beginning helps 
-            // us detect dates without years
-            var theDate = dayjs("1600-".concat(input));
-            if (theDate.isValid()) {
-                return { dateAnnual: theDate.format('MM-DD') };
-            }
-        } else {
-
-            // date with a year
-            theDate = dayjs(input);
-            if (theDate.isValid()) {
-                return { dateOnce: theDate.format('YYYY-MM-DD') };
-            }
-        }
+  if (typeof input === "string") {
+    // certain constants
+    if (input.toLowerCase() === "daily") {
+      return { recurAfter: 1 };
+    }
+    if (input.toLowerCase() === "monthly") {
+      return { dayOfMonth: 1 };
     }
 
-    return {};      // no parse possible
+    // day of week
+    const dayOfWeek = dayNameToWeekday(input);
+    if (dayOfWeek !== -1) {
+      return { dayOfWeek: dayOfWeek };
+    }
 
+    // date without a year
+    if (input.length <= 5) {
+      // concatenating 1600 to the beginning helps
+      // us detect dates without years
+      var theDate = dayjs("1600-".concat(input));
+      if (theDate.isValid()) {
+        return { dateAnnual: theDate.format("MM-DD") };
+      }
+    } else {
+      // date with a year
+      theDate = dayjs(input);
+      if (theDate.isValid()) {
+        return { dateOnce: theDate.format("YYYY-MM-DD") };
+      }
+    }
+  }
+
+  return {}; // no parse possible
 }
