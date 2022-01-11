2d43535a2ceccb86516d88c8ca3684b40a2d911c
Graham Charles <grahampcharles@gmail.com>
2022-01-10 20:14:16 -0800

    added typings

--- src/taskpaper.d.ts
+++ src/taskpaper.d.ts
@@ -1,6 +1,17 @@
-declare module 'taskpaper' {
-    function parse(
-        inputString: string
-    ): any;
+declare module "taskpaper" {
+    type TaskPaperNode = {
+        type: string;
+        value?: string;
+        children?: TaskPaperNode[];
+        tags?: string[];
+        depth: number;
+        index?: TaskPaperIndex;
+    };
+    type TaskPaperIndex = {
+        line: number;
+        column: number;
+    };
+
+    function parse(inputString: string): TaskPaperNode;
     export = parse;
 }
