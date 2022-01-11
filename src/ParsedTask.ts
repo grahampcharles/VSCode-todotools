5fe08a24e9b8431c685b90e2557caa45cf380b6e
Graham Charles <grahampcharles@gmail.com>
2022-01-10 19:52:09 -0800

    defined ParsedTask

--- src/ParsedTask.ts
+++ src/ParsedTask.ts
@@ -1,21 +1,22 @@
-import { TreeItem } from "vscode";
-
-
-
 export type ParsedTaskInput = {
-    type: string,
-    value?: string,
-    tags: string[]
+    type: string;
+    value: string;
+    tags: string[];
 };
 
 export interface TagWithValue {
-    tag: string,
-    value: string | undefined
-};
-
-
+    tag: string;
+    value: string | undefined;
+}
 
 export class ParsedTask {
+    private _value: string = "";
+    public get value(): string {
+        return this._value;
+    }
+    public set value(v: string) {
+        this._value = v;
+    }
 
     private _tags: TagWithValue[] | undefined;
     public get tags(): TagWithValue[] | undefined {
@@ -26,23 +27,26 @@ export class ParsedTask {
     }
 
     constructor(parsedtask: ParsedTaskInput) {
-
-
+        this.value = parsedtask.value;
+        this.tags = parsedtask.tags;
     }
-
 }
 
-export function parseTagValues(v: TagWithValue[] | undefined | string[]): TagWithValue[] | undefined {
-
-    if (v === undefined) { return undefined; };
+export function parseTagValues(
+    v: TagWithValue[] | undefined | string[]
+): TagWithValue[] | undefined {
+    if (v === undefined) {
+        return undefined;
+    }
 
     return v.map((item) => {
-        if (typeof item === 'string') { return parseTagValue(item); };
+        if (typeof item === "string") {
+            return parseTagValue(item);
+        }
         return item;
     });
 }
 export function parseTagValue(v: string): TagWithValue {
-
     // TODO: improve this with a regex
     const paren = v.indexOf("(");
 
@@ -50,5 +54,8 @@ export function parseTagValue(v: string): TagWithValue {
         return { tag: v } as TagWithValue;
     }
 
-    return { tag: v.substring(0, paren), value: v.substring(paren + 1, v.length - 1) };
+    return {
+        tag: v.substring(0, paren),
+        value: v.substring(paren + 1, v.length - 1),
+    };
 }
